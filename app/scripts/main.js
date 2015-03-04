4953cbeb34490e695e7148633e9b4b292d2ee516
Nikolay <Nikolay.Kozhuharenko@gmail.com>
2015-03-04 16:31:58 +0200

    add-proposal-finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,12 +3,17 @@
 "use strict";
 
 
-$('.slide-left').on('click', function () {
-  console.log($('.filter'));
-  console.log($('.slide-left'));
-
+$(document).ready(function(){
+	$(".add-subjects-button").click(function(){
+		$("#subject-modal").modal('show');
+	});
+	console.log($("#quota-modal"))
+	$(".add-quota-button").click(function(){
+		$("#quota-modal").modal('show');
+	});
 });
 
-// http://api.jqueryui.com/slide-effect/
+
+
 
 }());
